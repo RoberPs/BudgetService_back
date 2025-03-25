e9e8df55acc1b69fed7546e4d2b31d4014beb6bc
Roberto Pastor <rpastor79s@gmail.com>
2025-03-25 08:54:03 +0100

    fix

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,11 @@
-//CONEXION BASE DE DATOS DE RENDER
 
 import { Sequelize } from 'sequelize-typescript'
 import dontenv from 'dotenv'
 dontenv.config()
 
 
-export const db = new Sequelize( process.env.DB_URL, {
+
+export const db = new Sequelize(process.env.DB_URL, {
 
     models: [__dirname + '/../models/**/*'],
     logging:false,
